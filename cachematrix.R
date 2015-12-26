e1804fbb1283dce19de9167a88adba392c02d568
Eric Cobb <ericdcobb@gmail.com>
2015-12-26 12:21:04 -0700

    Initial commit. Functions are working. Adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(storedMatrix = matrix()) {
+    cachedInverse <- NULL
+    set <- function(y) {
+        storedMatrix <<- y
+        cachedInverse <<- NULL
+    }
+    get <- function() storedMatrix
+    
+    setInverse <- function(inverse) cachedInverse <<- inverse
+    getInverse <- function() cachedInverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachedMatrix, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inverseMatrix <- cachedMatrix$getInverse()
+    if(!is.null(inverseMatrix)) {
+        message("getting cached data")
+        return(inverseMatrix)
+    }
+    message("No cached inverse found, calculating and storing...")
+    storedMatrix <- cachedMatrix$get()
+    inverse <- solve(storedMatrix, ...)
+    cachedMatrix$setInverse(inverse)
+    inverse
 }
